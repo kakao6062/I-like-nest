5590daac74312ecd73c2b7f6380941cc099a2f13
kakao <kakaokaka6062@gmail.com>
2023-04-21 22:21:58 +0900

    入力を返すように変更

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,13 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "ilikenest" is now active!');
 
-	let disposable = vscode.commands.registerCommand('ilikenest.helloWorld', () => {
-
-		vscode.window.showInformationMessage('Hello World from ilikenest!');
+	let disposable = vscode.commands.registerCommand('ilikenest.helloWorld', async() => {
+		const limitNest = await vscode.window.showInputBox({
+			title: "ネストの深さを設定してください．"
+		});
+		if (limitNest !== undefined) {
+			vscode.window.showInformationMessage(`${limitNest}を受け付けました．`);
+		}
 	});
 
 	context.subscriptions.push(disposable);
