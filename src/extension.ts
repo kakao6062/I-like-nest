aaf1fb288d074515af15715d9eb3193fbabdfdbf
kakao <kakaokaka6062@gmail.com>
2023-04-22 11:40:40 +0900

    ネストを計測するコマンドを作成

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,42 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "ilikenest" is now active!');
+	let limitNest: string|undefined;
 
-	let disposable = vscode.commands.registerCommand('ilikenest.helloWorld', async() => {
-		const limitNest = await vscode.window.showInputBox({
-			title: "ネストの深さを設定してください．"
+	let setNest = vscode.commands.registerCommand('setNest', async() => {
+		limitNest = await vscode.window.showInputBox({
+			title: "Please set max nest size."
 		});
+
 		if (limitNest !== undefined) {
-			vscode.window.showInformationMessage(`${limitNest}を受け付けました．`);
+			vscode.window.showInformationMessage(`Max nest size : ${limitNest}`);
 		}
 	});
+	context.subscriptions.push(setNest);
+	
+	let displayNest = vscode.commands.registerCommand('displayNest', () =>{
+		const codeTexts = vscode.window.activeTextEditor?.document.getText();
+		let countNest = 0;
+		let output = 0;
+		let flag = true;
+		if (codeTexts !== undefined){
+			for (let i = 0; i < codeTexts?.length; i++) {
+				if (codeTexts.charAt(i) === " " && flag) {
+					countNest += 1;
+				}else if (codeTexts.charAt(i) === "\n") {
+					flag = true;
+					output = Math.max(output, countNest);
+					countNest = 0;
+				}else{
+					flag = false;
+				}
+			}
+		}
 
-	context.subscriptions.push(disposable);
+		vscode.window.showInformationMessage(`Now max nest size : ${output}`);
+	});
+	context.subscriptions.push(displayNest);
+
 }
 
 export function deactivate() {}
