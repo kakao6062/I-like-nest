492119760e5b8cdeb8aa3d50e4727e7b43bf477d
kakao <kakaokaka6062@gmail.com>
2023-04-24 21:30:26 +0900

    クラス設計 & activate関数整理

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,40 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let limitNest: string|undefined;
+	const ins = new ILikeNest();
 
-	let setNest = vscode.commands.registerCommand('setNest', async() => {
-		limitNest = await vscode.window.showInputBox({
+	let setNest = vscode.commands.registerCommand('extention.setNest', () =>{
+		ins.setNest();
+	});
+	
+	let displayNest = vscode.commands.registerCommand('extention.displayNest', () =>{
+		ins.displayNest();
+	});
+
+	context.subscriptions.push(ins);
+	context.subscriptions.push(setNest);
+	context.subscriptions.push(displayNest);
+}
+
+class ILikeNest {
+	private _disposable: vscode.Disposable;
+	private limit: string|undefined;
+
+	constructor() {
+		let subscriptions: vscode.Disposable[] = [];
+		this._disposable = vscode.Disposable.from(...subscriptions);
+	}
+
+	public async setNest() {
+		this.limit = await vscode.window.showInputBox({
 			title: "Please set max nest size."
 		});
-
-		if (limitNest !== undefined) {
-			vscode.window.showInformationMessage(`Max nest size : ${limitNest}`);
+		if (this.limit !== undefined) {
+			vscode.window.showInformationMessage(`Max nest size : ${this.limit}`);
 		}
-	});
-	context.subscriptions.push(setNest);
-	
-	let displayNest = vscode.commands.registerCommand('displayNest', () =>{
+	}
+
+	public displayNest() {
 		const codeTexts = vscode.window.activeTextEditor?.document.getText();
 		let countNest = 0;
 		let output = 0;
@@ -34,9 +54,10 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		vscode.window.showInformationMessage(`Now max nest size : ${output}`);
-	});
-	context.subscriptions.push(displayNest);
-
+	}
+	dispose() {
+		this._disposable.dispose();
+	}
 }
 
 export function deactivate() {}
