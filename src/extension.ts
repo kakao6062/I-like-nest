b5e29c405839c19f5fef081779a68c4e8c16677e
kakao <kakaokaka6062@gmail.com>
2023-04-30 04:26:06 +0900

    リファクタリング

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 		ins.displayNest(display);
 	});
 
-	let setNest = vscode.commands.registerCommand('extention.setNest', () =>{
+	let setNest = vscode.commands.registerCommand("extention.setNest", () =>{
 		ins.setNest();
 	});
 
@@ -21,60 +21,60 @@ export function activate(context: vscode.ExtensionContext) {
 
 class ILikeNest {
 	private _disposable: vscode.Disposable;
-	private limit: string|undefined;
+	private limit: number = 0;
 
 	constructor() {
-		let subscriptions: vscode.Disposable[] = [];
+		const subscriptions: vscode.Disposable[] = [];
 		this._disposable = vscode.Disposable.from(...subscriptions);
 	}
 
 	public async setNest() {
-		this.limit = await vscode.window.showInputBox({
+		const lim_str = await vscode.window.showInputBox({
 			title: "Please set max nest size."
 		});
-		if (this.limit !== undefined) {
-			vscode.window.showInformationMessage(`Max nest size : ${this.limit}`);
-		}
+
+		if (!lim_str) return;
+
+		this.limit = parseInt(lim_str);
+		vscode.window.showInformationMessage(`Max nest size : ${this.limit}`);	
 	}
 
 	public displayNest(display: vscode.StatusBarItem) {
 		const activeEditor = vscode.window.activeTextEditor;
-		const codeTexts = activeEditor?.document.getText();
-		const tabLength = activeEditor?.options.tabSize as number;
+
+		if (!activeEditor) return;
+
+		const codeTexts = activeEditor.document.getText();
+		const tabLength = activeEditor.options.tabSize as number;
 		const cats = [
 			'https://media.tenor.com/vR2nHwWXla0AAAAC/oh-no-cat-falling.gif',
 			'https://media.tenor.com/Jat_4J29fygAAAAC/cat-stairs.gif',
 			'https://media.tenor.com/r293RsMY9-AAAAAd/cat.gif'
 		]
 
-		let countNest = 0;
-		let flag = true;
 		let maxNestSize = 0;
-		if (codeTexts !== undefined){
-			for (let i = 0; i < codeTexts?.length; i++) {
-				if ((codeTexts.charAt(i) === " " || codeTexts.charAt(i) === "\t") && flag) {
-					if (codeTexts.charAt(i) === " ") {
-						countNest += 1;
-					}
-					else {
-						countNest += tabLength
-					}
-				}else if (codeTexts.charAt(i) === "\n") {
-					flag = true;
-					maxNestSize = Math.max(maxNestSize, countNest);
-					countNest = 0;
-				}else{
-					flag = false;
-				}
-			}
-			display.text = `Nest Size: ${maxNestSize}`;
-			display.show();
-		}else {
+		if (!codeTexts){
 			display.hide();
+			return;
 		}
 
-		function getWebviewContent(){
-			return `<!DOCTYPE html>
+
+		for (const row of codeTexts.split("\n")) {
+			let countNest = 0;
+			for (const c of row) {
+				if (!( c in [" ", "\t"])) break;
+					countNest += (c === " ") ? 1: tabLength;
+			}
+					maxNestSize = Math.max(maxNestSize, countNest);
+		}
+
+		display.text = `Nest Size: ${maxNestSize}`;
+		display.show();
+
+
+
+		const getWebviewContent = (() =>
+			`<!DOCTYPE html>
 			<html lang="ja">
 			<head>
 			<meta charset="UTF-8">
@@ -83,20 +83,19 @@ class ILikeNest {
 			<body>
 			<img src='${cats[Math.floor(Math.random() * cats.length)]}' />
 			</body>
-			</html>`;
-		}
+			</html>`
+		);
 
-		if(this.limit !== undefined){
-			if(maxNestSize > parseInt(this.limit)) {
+		if(!this.limit) return;
 
-				const panel = vscode.window.createWebviewPanel(
-					'Your Nest as Cat',
-					'Cat is falling as your nest',
-					vscode.ViewColumn.Two,
-					{}
-				);
-				panel.webview.html = getWebviewContent();
-			} 
+		if(maxNestSize > this.limit) {
+			const panel = vscode.window.createWebviewPanel(
+				'Your Nest as Cat',
+				'Cat is falling as your nest',
+				vscode.ViewColumn.Two,
+				{}
+			);
+			panel.webview.html = getWebviewContent();
 		}
 	}
 
